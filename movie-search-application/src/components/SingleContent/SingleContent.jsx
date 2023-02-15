27bebc5aae1075cb2f6eece793f620801dbdd81f
Johno <john@mount-media-online.com>
2023-02-15 11:24:56 +0000

    Mui Rating added

--- movie-search-application/src/components/SingleContent/SingleContent.jsx
+++ movie-search-application/src/components/SingleContent/SingleContent.jsx
@@ -2,6 +2,8 @@ import { AlertTitle } from '@mui/material'
 import React from 'react'
 import { img_300, unavailable } from '../../config/config'
 import './SingleContent.css'
+import { Badge } from '@mui/material'
+
 
 const SingleContent = ({
   id,
@@ -13,6 +15,7 @@ const SingleContent = ({
 }) => {
   return (
     <div className='media'>
+      <Badge badgeContent={vote_average} color={vote_average > 6 ? 'primary': 'secondary'}/>
       <img className='poster' src={poster? `${img_300}${poster}` : unavailable} alt={title}  />
       <b className='title'>{title}</b>
       <span className='subTitle'>{media_type === "tv" ? "Tv Series" : "Movie" }
