70e4e97e6e9a8ce647a05466f23f016b616c385f
Johno <john@mount-media-online.com>
2023-02-15 10:57:58 +0000

    useScroll Hook added

--- movie-search-application/src/components/header/Header.jsx
+++ movie-search-application/src/components/header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css'
+import { useScrollTo } from "react-use-window-scroll";
 
 const Header = () => {
+    const scrollTo = useScrollTo();
     return (
         <div>
-        <span className='header'>Vision Box</span>
+        <span onClick={() => scrollTo({ top: 0, left: 0, behavior: "smooth" })} className='header'>Vision Box</span>
         </div>
     )
 }
